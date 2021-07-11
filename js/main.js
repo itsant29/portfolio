14b34ed5300f0be8753342c21b7dbbea657d5c2e
Antoinette <antoinettehall29@gmail.com>
2021-07-11 11:23:03 +0800

    update skills grid

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ let date = new Date();
 let yearPos = document.getElementById("getYear");
 let currYear = date.getFullYear();
 yearPos.innerHTML = currYear;
+
+//function getAge(dateString) 
+//{
+  //  var today = new Date();
+    //var birthDate = new Date(dateString);
+//    var age = today.getFullYear() - birthDate.getFullYear();
+//    var m = today.getMonth() - birthDate.getMonth();
+  //  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+    //{
+   //     age--;
+    //}
+   // return age;
+//}
+
